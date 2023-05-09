a95bc4d372ef859156cb3d5dc813ac5a92f3c364
junhual1 <junhual1@student.unimelb.edu.au>
2023-05-09 22:21:18 +1000

    mastodown support

--- ansible/run-mrc.sh
+++ ansible/run-mrc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # . ./openrc.sh; ansible-playbook -i hosts -vv mrc.yaml --ask-become-pass | tee output.txt
-. ./openrc.sh; ansible-playbook -vv mrc.yaml | tee output.txt
+. ./openrc.sh; ansible-playbook -i hosts -vv mrc.yaml | tee output.txt
